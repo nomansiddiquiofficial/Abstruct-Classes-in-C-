362e9757ca30212037019e578c42dbcfea63f3d3
Noman Siddiqui <92144508+nomansiddiquiofficial@users.noreply.github.com>
2024-05-14 10:26:33 +0500

    Add project files.

---
+++ Abstruct Classes/Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+ abstract class Person
+{
+    public string name;
+    public int age;
+    public abstract void printdetails(string name, int age); 
+    
+}
+class Student : Person
+{
+    public int rollNo;
+    public double fees;
+    public override void printdetails(string name , int age)
+    {
+        this.name = name;
+        this.age = age;
+        Console.WriteLine($"the name is: {name} and the age is :{age}");
+    }
+}
+
+
+class Program
+{
+    public static void Main()
+    {
+        Student student = new Student();
+        Person person = new Student();
+        person.name = "Noman";
+        person.age = 20;
+        Console.WriteLine(person.name,person.age);
+
+
+        student.rollNo = 15;
+        student.fees = 3000;
+        student.printdetails("Ali", 20);
+        Console.WriteLine(student.rollNo);
+        Console.WriteLine(student.fees);
+    }
+}
